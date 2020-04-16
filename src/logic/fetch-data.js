a2ce4967923e246655538debd0a2aba3a9b8c65d
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-04-16 19:45:03 +0300

    feat: add a button that changes the country name it shows after clicking it. the countries names come from the response from the api

---
+++ src/logic/fetch-data.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const getOnlineWorldwideStatistics = async () => {
+    const response = await axios.get('https://covid-193.p.rapidapi.com/statistics', {
+        headers: {
+            'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+            'x-rapidapi-key': 'd7382f77damsh1447cc50e9c4f49p1b3e7cjsn48d3d0e9d300'
+        }
+    });
+
+    return response.data;
+}
+
+export default getOnlineWorldwideStatistics;
