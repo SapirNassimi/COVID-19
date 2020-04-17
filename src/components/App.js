0f8c55bfe621acf594ea500e8ca24308b1cd215d
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-04-17 15:46:36 +0300

    feat: add continents data state and remove then from countries info

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,48 @@
 import React, { useState, useEffect } from 'react';
 
-import getOnlineWorldwideStatistics from '../logic/fetch-data';
+import getOnlineWorldwideStatistics from '../logic/fetch-statistics';
+import getCountriesNames from '../logic/fetch-countries';
+
 import StatsDataTable from './StatsDataTable';
 
 const App = () => {
-    const [tableData, setTableData] = useState({
-        response: []
-    });
+    const [allDataFromServer, setAllDataFromServer] = useState([]);
+    const [countriesData, setCountriesData] = useState([]);
+    const [continentsData, setContinentsData] = useState([]);
+    const [countriesNames, setCountriesNames] = useState([]);
 
-    const loadData = async () => {
+    const loadCountriesNames = async () => {
+        const countriesNamesResponse = await getCountriesNames();
+        setCountriesNames(countriesNamesResponse);
+    }
+
+    const loadAllStatsFromServer = async () => {
         const data = await getOnlineWorldwideStatistics();
-
-        setTableData(data);
+        setAllDataFromServer(data);
     }
 
     useEffect(() => {
-        loadData();
+        const tempCountries = [];
+        const tempContinents = [];
+
+        allDataFromServer.map(record => {
+            countriesNames.includes(record.country) ?
+                tempCountries.push(record) :
+                tempContinents.push(record);
+        });
+
+        setCountriesData(tempCountries);
+        setContinentsData(tempContinents);
+
+    }, [countriesNames, allDataFromServer]);
+
+    useEffect(() => {
+        loadCountriesNames();
+        loadAllStatsFromServer();
     }, []);
 
     return (
-        <StatsDataTable data={tableData}/>
+        <StatsDataTable data={countriesData}/>
     );
 }
 
