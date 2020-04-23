e3982753d2e0c109d371dded60b6bad7a355115f
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-04-24 01:48:16 +0300

    style: replace header with styled components

--- src/components/StatsDataItem.js
+++ src/components/StatsDataItem.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import styled from 'styled-components'
+
+import { ListItem } from './ListItem';
+import { Column } from './Column';
 
 const StatsDataItem = props => {
     const data = props.data;
-
-    const ListItem = styled.li`
-        border-radius: 10px;
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 10px;
-        padding-left: 30px;
-        padding-right: 30px;
-    `;
-
-    const Column = styled.div`
-        flex-basis: ${props => props.country ? '16%' : '12%'};
-        color: ${props => props.inputColor || '#333'};
-
-        ${props => props.country && `
-            font-weight: bolder;
-        `};
-    `;
 
     return (
         <ListItem>
