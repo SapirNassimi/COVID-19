18f44ac5800e81458194895259134fd9d744db74
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-04-24 01:47:39 +0300

    feat: export list item and column to individual files

---
+++ src/components/ListItem.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ListItem = styled.li`
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding-left: 30px;
+    padding-right: 30px;
+    background-color: #fff;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    box-shadow: 0px 0px 9px 2px rgba(0, 0, 0, 0.1);
+`;
