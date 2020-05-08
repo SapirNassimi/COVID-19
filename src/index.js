ee0bf9416f6a8dcf5129b6ddfba7ccce11b9a038
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-05-08 14:30:51 +0300

    change index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 
 import App from './containers/App';
 
-const store = createStore(reducer);
-//store.dispatch(getAllData());
-
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
