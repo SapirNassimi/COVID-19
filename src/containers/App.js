5e045e5f4ee9e5ded2331ee83a846a444a38dc72
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-05-08 15:34:44 +0300

    feat: load data to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { getCountriesData, getContinentsData, getGlobalData } from '../api/data';
 
@@ -10,15 +10,26 @@ const App = () => {
     const [continentsData, setContinentsData] = useState([]);
     const [globalData, setGlobalData] = useState({});
 
+    useEffect(() => {
+        retriveAppData();
+    }, []);
+
     const retriveAppData = async () => {
-        setCountriesData(getCountriesData());
-        setContinentsData(getContinentsData());
+        const countriesFromServer = await getCountriesData();
+        const continentsFromServer = await getContinentsData();
+        const globalFromServer = await getGlobalData();
+
+        setCountriesData(countriesFromServer);
+        setContinentsData(continentsFromServer);
+        setGlobalData(globalFromServer);
     }
 
     return (
         <div>
-            <PageHeader />
-            <DataTable />
+            <PageHeader globalData={globalData} />
+            <DataTable countriesData={countriesData}
+                continentsData={continentsData}
+                globalData={globalData} />
         </div>
     )
 }
